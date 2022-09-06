527108efd3ed67c1e131ff0bf434d4bd1826f0d2
CrimsonCoder42 <nostro37@gmail.com>
2022-09-06 15:43:09 -0400

    case classes

--- src/main/scala/OOP.scala
+++ src/main/scala/OOP.scala
@@ -1,5 +1,29 @@
 object OOP extends App {
 
+  class Animal {
+    // def fields
+    val age: Int = 0
+
+    // def methods
+    def eat() = println("I'm eating")
+
+  }
+
+  val anAnimal = new Animal
+
+  // inheritance
+
+  class Dog(val name: String) extends Animal {
+
+  }
+
+  val aDog = new Dog("Lassie")
+
+  aDog.eat()
+
+  val myAnimal: Animal = new Dog("Devin")
+
+  myAnimal.eat()
 
 
 }
